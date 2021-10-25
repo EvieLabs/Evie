c4ff54932779c3c98925c9dace04680b8e987cde
Tristan <69066026+twisttaan@users.noreply.github.com>
2021-10-25 19:30:38 +1100

    bye bye bin

--- bin/events/interactionCreate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = {
-    name: "interactionCreate",
-    execute(interaction) {
-        const { axo } = require("../axologs");
-        axo.i(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-    },
-};
