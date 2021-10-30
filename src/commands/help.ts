0e37273d8260e893e6917f8cee22bced34d1fecb
Tristan <69066026+twisttaan@users.noreply.github.com>
2021-10-31 10:53:46 +1100

    Help Command

---
+++ src/commands/help.ts
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Need help with me?"),
+  async execute(interaction) {
+    const exampleEmbed = new MessageEmbed().setColor("#0099ff").setTimestamp();
+    try {
+      exampleEmbed.setDescription(
+        `Hey do you need help with my commands? If so you can find a list by doing this, if you need even more help you can read more info [here](https://discord.gg/PYdASSE2gr) and also talk to our staff!`
+      );
+      exampleEmbed.setImage(
+        `https://cdn.discordapp.com/attachments/885135206435151872/903570072675692555/RqivnDqPvH.gif`
+      );
+      interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
