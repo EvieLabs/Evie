ec15d926fd6c25b7c95b66a04cf726e3b5a37354
Tristan <69066026+twisttaan@users.noreply.github.com>
2021-10-31 17:17:15 +1100

    COMMAND FIX ASAP

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,7 +8,7 @@ module.exports = {
 
     // Evie Doesn't Have time for you ;)
 
-    const anyNewCommands: Boolean = false;
+    const anyNewCommands: Boolean = true;
 
     client.user.setPresence({
       status: "dnd",
