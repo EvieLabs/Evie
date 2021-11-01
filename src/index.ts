fd4fb7f75bf749172ed7622b1e2b0ccb1446eec5
Tristan <69066026+twisttaan@users.noreply.github.com>
2021-11-02 07:43:39 +1100

    a

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,14 @@ import * as config3 from "./botconfig/filters.json";
 import * as config2 from "./botconfig/embed.json";
 import * as config4 from "./botconfig/settings.json";
 
-// Create a new Player (you don't need any API Key)
+const DBD = require("discord-dashboard");
+const CaprihamTheme = require("dbd-capriham-theme");
+
+let langsSettings = {};
+
+let currencyNames = {};
+
+let botNicknames = {};
 
 // create a new Discord client
 
@@ -43,6 +50,139 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
+
+// Dashboard
+const Dashboard = new DBD.Dashboard({
+  port: 80,
+  client: {
+    id: "clientId",
+    secret: "clientSecret",
+  },
+  redirectUri: "http://localhost/discord/callback",
+  domain: "http://localhost",
+  bot: client,
+  theme: CaprihamTheme({
+    websiteName: "Assistants",
+    iconURL: "https://assistants.ga/ac_logo_v6.png",
+    index: {
+      card: {
+        title: "Assistants - The center of everything",
+        description:
+          "Assistants Discord Bot management panel. Assistants Bot was created to give others the ability to do what they want. Just.<br>That's an example text.<br><br><b><i>Feel free to use HTML</i></b>",
+        image:
+          "https://www.geeklawblog.com/wp-content/uploads/sites/528/2018/12/liprofile-656x369.png",
+      },
+      information: {
+        title: "Information",
+        description:
+          "To manage your bot, go to the <a href='/manage'>Server Management page</a>.<br><br>For a list of commands, go to the <a href='/commands'>Commands page</a>.<br><br><b><i>You can use HTML there</i></b>",
+      },
+      feeds: {
+        title: "Feeds",
+        list: [
+          {
+            icon: "fa fa-user",
+            text: "New user registered",
+            timeText: "Just now",
+            bg: "bg-light-info",
+          },
+          {
+            icon: "fa fa-server",
+            text: "Server issues",
+            timeText: "3 minutes ago",
+            bg: "bg-light-danger",
+          },
+        ],
+      },
+    },
+    commands: {
+      pageTitle: "Commands",
+      table: {
+        title: "List",
+        subTitle: "All Assistants' commands",
+        list: [
+          {
+            commandName: "Test command",
+            commandUsage: "prefix.test <arg> [op]",
+            commandDescription: "Lorem ipsum dolor sth",
+          },
+          {
+            commandName: "2nd command",
+            commandUsage: "oto.nd <arg> <arg2> [op]",
+            commandDescription: "Lorem ipsum dolor sth, arg sth arg2 stuff",
+          },
+        ],
+      },
+    },
+  }),
+  settings: [
+    {
+      categoryId: "setup",
+      categoryName: "Setup",
+      categoryDescription: "Setup your bot with default settings!",
+      categoryOptionsList: [
+        {
+          optionId: "lang",
+          optionName: "Language",
+          optionDescription: "Change bot's language easily",
+          optionType: DBD.formTypes.select({
+            Polish: "pl",
+            English: "en",
+            French: "fr",
+          }),
+          getActualSet: async ({ guild }) => {
+            return langsSettings[guild.id] || null;
+          },
+          setNew: async ({ guild, newData }) => {
+            langsSettings[guild.id] = newData;
+            return;
+          },
+        },
+        {
+          optionId: "nickname",
+          optionName: "Nickname",
+          optionDescription: "Bot's nickname on the guild",
+          optionType: DBD.formTypes.input("Bot username", 1, 16, false, true),
+          getActualSet: async ({ guild }) => {
+            return botNicknames[guild.id] || false;
+          },
+          setNew: async ({ guild, newData }) => {
+            botNicknames[guild.id] = newData;
+            return;
+          },
+        },
+      ],
+    },
+    {
+      categoryId: "eco",
+      categoryName: "Economy",
+      categoryDescription: "Economy Module Settings",
+      categoryOptionsList: [
+        {
+          optionId: "currency_name",
+          optionName: "Currency name",
+          optionDescription: "Economy module Guild currency name",
+          optionType: DBD.formTypes.input(
+            "Currency name",
+            null,
+            10,
+            false,
+            true
+          ),
+          getActualSet: async ({ guild }) => {
+            return currencyNames[guild.id] || null;
+          },
+          setNew: async ({ guild, newData }) => {
+            currencyNames[guild.id] = newData;
+            return;
+          },
+        },
+      ],
+    },
+  ],
+});
+
+Dashboard.init();
 
 // discord-music-player
 
