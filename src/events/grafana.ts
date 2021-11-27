977a1d44b6d81f2d595bc3195ecd3f7f87cbe658
Tristan Camejo <twisttaan@icloud.com>
2021-11-27 20:13:17 +1100

    feat: better backend prom-client

--- src/events/grafana.ts
+++ src/events/grafana.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import server from "express";
+import cluster from "cluster";
 import { collectDefaultMetrics, register } from "prom-client";
 module.exports = {
   name: "ready",
@@ -6,17 +7,107 @@ module.exports = {
   async execute(client) {
     collectDefaultMetrics();
 
-    const app = express();
+    // Create custom metrics
 
-    app.get("/metrics", async (_req, res) => {
-      try {
-        res.set("Content-Type", register.contentType);
-        res.end(await register.metrics());
-      } catch (err) {
-        res.status(500).end(err);
+    const Histogram = require("../").Histogram;
+    const h = new Histogram({
+      name: "test_histogram",
+      help: "Example of a histogram",
+      labelNames: ["code"],
+    });
+
+    const Counter = require("../").Counter;
+    const c = new Counter({
+      name: "test_counter",
+      help: "Example of a counter",
+      labelNames: ["code"],
+    });
+
+    new Counter({
+      name: "scrape_counter",
+      help: "Number of scrapes (example of a counter with a collect fn)",
+      collect() {
+        // collect is invoked each time `register.metrics()` is called.
+        this.inc();
+      },
+    });
+
+    const Gauge = require("../").Gauge;
+    const g = new Gauge({
+      name: "test_gauge",
+      help: "Example of a gauge",
+      labelNames: ["method", "code"],
+    });
+
+    // Set metric values to some random values for demonstration
+
+    setTimeout(() => {
+      h.labels("200").observe(Math.random());
+      h.labels("300").observe(Math.random());
+    }, 10);
+
+    setInterval(() => {
+      c.inc({ code: 200 });
+    }, 5000);
+
+    setInterval(() => {
+      c.inc({ code: 400 });
+    }, 2000);
+
+    setInterval(() => {
+      c.inc();
+    }, 2000);
+
+    setInterval(() => {
+      g.set({ method: "get", code: 200 }, Math.random());
+      g.set(Math.random());
+      g.labels("post", "300").inc();
+    }, 100);
+
+    if (cluster.isWorker) {
+      // Expose some worker-specific metric as an example
+      setInterval(() => {
+        c.inc({ code: `worker_${cluster.worker!.id}` });
+      }, 2000);
+    }
+
+    const t = [];
+    setInterval(() => {
+      for (let i = 0; i < 100; i++) {
+        // @ts-ignore
+        t.push(new Date());
+      }
+    }, 10);
+    setInterval(() => {
+      while (t.length > 0) {
+        t.pop();
       }
     });
 
-    app.listen(4001, "0.0.0.0");
+    // Setup server to Prometheus scrapes:
+
+    server.get("/metrics", async (req, res) => {
+      try {
+        res.set("Content-Type", register.contentType);
+        res.end(await register.metrics());
+      } catch (ex) {
+        res.status(500).end(ex);
+      }
+    });
+
+    server.get("/metrics/counter", async (req, res) => {
+      try {
+        res.set("Content-Type", register.contentType);
+        res.end(await register.getSingleMetricAsString("test_counter"));
+      } catch (ex) {
+        res.status(500).end(ex);
+      }
+    });
+
+    const port = process.env.PORT || 4001;
+    console.log(
+      `Server listening to ${port}, metrics exposed on /metrics endpoint`
+    );
+    server.listen(port);
   },
 };
