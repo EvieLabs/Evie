b3abe8413b89a331eb372dde868a9d70a15192d7
Tristan Camejo <twisttaan@icloud.com>
2021-11-27 19:35:40 +1100

    feat: grafana

---
+++ src/events/grafana.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { collectDefaultMetrics, register } from "prom-client";
+module.exports = {
+  name: "ready",
+  once: true,
+  async execute(client) {
+    collectDefaultMetrics();
+
+    const app = express();
+
+    app.get("/metrics", async (_req, res) => {
+      try {
+        res.set("Content-Type", register.contentType);
+        res.end(await register.metrics());
+      } catch (err) {
+        res.status(500).end(err);
+      }
+    });
+
+    app.listen(4001, "0.0.0.0");
+  },
+};
