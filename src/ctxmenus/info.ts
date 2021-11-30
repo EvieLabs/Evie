9b4ed95c34fdf30d635e8dd722d97f0de479673a
Tristan Camejo <twisttaan@icloud.com>
2021-11-30 21:33:18 +1100

    feat: Make user info do something

--- src/ctxmenus/info.ts
+++ src/ctxmenus/info.ts
@@ -1,12 +1,7 @@
 import { embed } from "../tools";
 import * as evie from "../tools";
 import { axo } from "../axologs";
-import {
-  ContextMenuInteraction,
-  MessageActionRow,
-  MessageButton,
-} from "discord.js";
-import { ApplicationCommandTypes } from "discord.js/typings/enums";
+import { ContextMenuInteraction, GuildMember, MessageButton } from "discord.js";
 
 module.exports = {
   data: {
@@ -14,6 +9,37 @@ module.exports = {
     type: 2,
   },
   async execute(i: ContextMenuInteraction) {
-    i.reply("soon!");
+    if (!i.inGuild()) {
+      return;
+    }
+    const m = i.options.getMember("user") as GuildMember;
+    const e = await evie.embed(i.guild!);
+    e.setTitle(`Member Info for ${m.user.tag}`);
+    e.setThumbnail(m.user.displayAvatarURL());
+    e.addField("User", `${m.user.tag} (${m.user.id})`);
+    e.addField("Nickname", m.nickname || "None");
+    e.addField(
+      `Joined ${m.guild.name}`,
+      m.joinedAt ? m.joinedAt.toLocaleString() : "Unknown"
+    );
+    e.addField(
+      `Created Account`,
+      m.user.createdAt ? m.user.createdAt.toLocaleString() : "Unknown"
+    );
+    e.addField("Bot", m.user.bot ? "Yes" : "No");
+    e.addField("Roles", m.roles.cache.map((r) => r.name).join(", "));
+    e.addField(
+      "Permissions",
+      capitalizeEachWord(
+        m.permissions.toArray().join(", ").replace(/\_/g, " ").toLowerCase()
+      )
+    );
+    i.reply({ embeds: [e] });
   },
 };
+
+function capitalizeEachWord(str: string) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
