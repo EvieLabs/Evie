3148297fd6647b90f84edf89cb6bfde8105d0fc5
Tristan Camejo <twisttaan@icloud.com>
2021-12-21 10:38:55 +1100

    tsmp: revoke

--- src/Ecommands/tsmp.ts
+++ src/Ecommands/tsmp.ts
@@ -16,7 +16,19 @@ module.exports = {
             .setDescription("Player to accept")
             .setRequired(true)
         )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("revoke")
+        .setDescription("Revoke a Player")
+        .addUserOption((option) =>
+          option
+            .setName("player")
+            .setDescription("Player to revoke")
+            .setRequired(true)
+        )
     ),
+
   async execute(i: CommandInteraction) {
     const subcommand = i.options.getSubcommand();
     if (subcommand == "accept") {
@@ -55,6 +67,46 @@ module.exports = {
         .add(r, `Accepted by ${i.user}`)
         .then(() => {
           i.reply({ embeds: [e], content: `${m} Good News!` });
+        })
+        .catch(() => {
+          i.reply({ content: "Failed! Tell Tristan asap", ephemeral: true });
+        });
+    } else if (subcommand == "revoke") {
+      if (!i.inGuild()) {
+        return;
+      }
+      const mem: GuildMember = i.member! as GuildMember;
+      if (!mem.roles.cache.has("819442569128706068")) {
+        return i.reply({
+          content: "You are not a staff member!",
+          ephemeral: true,
+        });
+      }
+      const m = i.options.getMember("user") as GuildMember;
+      if (!m.roles.cache.has("878074525223378974")) {
+        return i.reply({
+          content: "You can only revoke players who have been accepted!",
+          ephemeral: true,
+        });
+      }
+      const r: Role = i.guild!.roles.cache.find(
+        (r) => r.id == "878074525223378974"
+      ) as Role;
+      const br: Role = i.guild!.roles.cache.find(
+        (r) => r.id == "904148775801585676"
+      ) as Role;
+      const e = await evie.embed(i.guild!);
+
+      e.setTitle("Revoked!");
+      e.setDescription(
+        `Oh no! ${m} You were revoked by ${i.user}, you can appeal later by making a ticket <#884223699778150400>`
+      );
+
+      await m?.roles
+        .remove(r, `Revoked by ${i.user}`)
+        .then(() => {
+          m?.roles.add(br, `Revoked by ${i.user}`);
+          i.reply({ embeds: [e], content: `${m} Oh!` });
         })
         .catch(() => {
           i.reply({ content: "Failed! Tell Tristan asap", ephemeral: true });
