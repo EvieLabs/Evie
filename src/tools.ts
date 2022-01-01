51a55bae2f41e6939ddc730a6f7a7a39c8ad353e
Tristan Camejo <twisttaan@icloud.com>
2022-01-01 15:16:23 +1100

    fix: firebase

--- src/tools.ts
+++ src/tools.ts
@@ -13,9 +13,7 @@ import fetch from "node-fetch";
 import { Model, Models } from "mongoose";
 import { initializeApp } from "firebase-admin/app";
 
-export const app = initializeApp({
-  credential: JSON.parse(process.env.FBA!),
-});
+export const app = initializeApp(JSON.parse(process.env.FBA!));
 
 // export tsmp
 
