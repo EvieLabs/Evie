c1756cd8f71e78575b1a19309e7db9412cdec9f1
Tristan Camejo <twisttaan@icloud.com>
2022-02-25 22:31:25 +1100

    feat: better preflight script

--- scripts/preflight.js
+++ scripts/preflight.js
@@ -18,9 +18,9 @@ const fs = require("fs");
 const glob = require("glob");
 const path = require("path");
 
-var getDirectories = function (src, callback) {
+function getDirectories(src, callback) {
   glob(src + "/**/*", callback);
-};
+}
 
 const header = `/* 
 Copyright 2022 Tristan Camejo
@@ -40,23 +40,39 @@ limitations under the License.
 
 `;
 
-// for every typescript file in the working directory add a copyright header
+function containsHeader(string) {
+  return string.match(/\\*\nCopyright /);
+}
+
 getDirectories(".", function (err, files) {
   if (err) {
     console.log("Error", err);
   } else {
-    files.forEach(function (file) {
-      if (path.extname(file) === ".ts") {
-        fs.readFile(file, "utf8", function (err, data) {
-          if (err) {
-            return console.log(err);
-          }
-          var result = header + data;
-          fs.writeFile(file, result, "utf8", function (err) {
-            if (err) return console.log(err);
+    const readAndAddHeader = new Promise((resolve, reject) => {
+      let editedFiles = 0;
+      let processedFiles = 0;
+
+      files.forEach(function (file) {
+        processedFiles++;
+        if (processedFiles === files.length) return resolve(editedFiles);
+        if (path.extname(file) === ".ts") {
+          fs.readFile(file, "utf8", function (err, data) {
+            if (err) {
+              return console.log(err);
+            }
+            if (containsHeader(data)) return;
+            const result = header + data;
+            fs.writeFile(file, result, "utf8", function (err) {
+              if (err) return console.log(err);
+              editedFiles++;
+              console.log(`Added copyright header to: ${file}`);
+            });
           });
-        });
-      }
+        }
+      });
+    });
+    readAndAddHeader.then((editedFiles) => {
+      console.log(`${editedFiles} files updated.`);
     });
   }
 });
