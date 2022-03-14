190a21ea2aeaf3ab2a82d34a6ecb5db71abc57e1
Tristan Camejo <twisttaan@icloud.com>
2022-03-14 11:16:04 +1100

    fix: head

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -8,40 +8,35 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <div>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/icons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/icons/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/icons/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
-          <link rel="shortcut icon" href="/icons/favicon.ico" />
-          <meta name="apple-mobile-web-app-title" content="Evie" />
-          <meta name="application-name" content="Evie" />
-          <meta name="msapplication-TileColor" content="#9f00a7" />
-          <meta
-            name="msapplication-config"
-            content="/icons/browserconfig.xml"
-          />
-          <meta name="theme-color" content="#ffffff" />
-        </div>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Evie" />
+        <meta name="application-name" content="Evie" />
+        <meta name="msapplication-TileColor" content="#9f00a7" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Banner />
       <Header />
