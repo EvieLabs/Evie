43d5b0c9aa126fc8b12b927556a8e7f617d300a5
Tristan Camejo <twisttaan@icloud.com>
2022-04-05 17:11:55 +1000

    remove: copyright on every file

--- apps/bot/src/classes/EviePunish.ts
+++ apps/bot/src/classes/EviePunish.ts
@@ -1,19 +1,3 @@
-/* 
-Copyright 2022 Team Evie
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 import { modAction } from "#root/utils/builders/stringBuilder";
 import { punishDB } from "#root/utils/database/punishments";
 import type { BanOptions, Guild, GuildMember, Snowflake } from "discord.js";
