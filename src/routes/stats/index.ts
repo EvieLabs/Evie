6140ac833acdea6621103ab74e43f7c7ead8faa2
Tristan Camejo <twisttaan@icloud.com>
2022-04-13 21:58:19 +1000

    feat: more stats

--- src/routes/stats/index.ts
+++ src/routes/stats/index.ts
@@ -6,16 +6,25 @@ import {
   Route,
   RouteOptions,
 } from "@sapphire/plugin-api";
+import { cpus } from "os";
+import * as pidusage from "pidusage";
 
 @ApplyOptions<RouteOptions>({ route: "/stats" })
 export class StatsRoute extends Route {
-  public [methods.GET](_request: ApiRequest, response: ApiResponse) {
+  public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
+    const stats = await pidusage.default(process.pid);
+
     response.json({
       users: this.container.client.guilds.cache.reduce(
         (acc, guild) => acc + guild.memberCount,
         0
       ),
-      servers: this.container.client.guilds.cache.reduce((acc) => acc + 1, 0),
+      guilds: this.container.client.guilds.cache.reduce((acc) => acc + 1, 0),
+      cpuUsage: parseFloat((stats.cpu / cpus().length).toFixed(2)),
+      wsPing: this.container.client.ws.ping,
+      unavailableGuilds: this.container.client.guilds.cache.filter(
+        (guild) => !guild.available
+      ).size,
     });
   }
 }
