16f4a4e4df8c254b633138ec0bea0932aa79196c
Tristan Camejo <twisttaan@icloud.com>
2022-04-14 10:28:17 +1000

    feat: @me

--- src/routes/users/@me/info.ts
+++ src/routes/users/@me/info.ts
@@ -1,8 +1,14 @@
+import {
+  flattenGuild,
+  flattenUser,
+  type FlattenedGuild,
+} from "#root/utils/api/ApiTransformers";
 import { authenticated } from "#root/utils/api/decorators";
 import { ApplyOptions } from "@sapphire/decorators";
 import {
   ApiRequest,
   ApiResponse,
+  HttpCodes,
   methods,
   Route,
   RouteOptions,
@@ -14,9 +20,15 @@ import {
 export class UserRoute extends Route {
   @authenticated()
   public async [methods.GET](request: ApiRequest, response: ApiResponse) {
+    const { client } = this.container;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const user = await this.container.client.users.fetch(request.auth!.id);
+    const user = await client.users.fetch(request.auth!.id).catch(() => null);
+    if (user === null) return response.error(HttpCodes.InternalServerError);
 
-    return response.json(user);
+    const guilds: FlattenedGuild[] = [];
+    for (const guild of client.guilds.cache.values()) {
+      if (guild.members.cache.has(user.id)) guilds.push(flattenGuild(guild));
+    }
+    return response.json({ ...flattenUser(user), guilds });
   }
 }
