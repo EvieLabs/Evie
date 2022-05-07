d28cd4eacc84eef860da5b597839f675ce366fbb
Tristan Camejo <twisttaan@icloud.com>
2022-05-07 19:54:08 +1000

    remove: cpu usage

--- src/routes/stats/index.ts
+++ src/routes/stats/index.ts
@@ -6,18 +6,13 @@ import {
   Route,
   RouteOptions,
 } from "@sapphire/plugin-api";
-import { cpus } from "os";
-import * as pidusage from "pidusage";
 
 @ApplyOptions<RouteOptions>({ route: "/stats" })
 export class StatsRoute extends Route {
   public async [methods.GET](_request: ApiRequest, response: ApiResponse) {
-    const stats = await pidusage.default(process.pid, { usePs: true });
-
     response.json({
       users: this.container.client.stats.users,
       guilds: this.container.client.stats.guilds,
-      cpuUsage: parseFloat((stats.cpu / cpus().length).toFixed(2)),
       wsPing: this.container.client.stats.wsPing,
     });
   }
