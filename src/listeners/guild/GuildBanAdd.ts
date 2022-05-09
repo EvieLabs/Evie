21ab077e6f57ad2390ebcb634ae528046751c2d0
Tristan Camejo <twisttaan@icloud.com>
2022-05-09 16:28:54 +1000

    fix: fetch audit log

--- src/listeners/guild/GuildBanAdd.ts
+++ src/listeners/guild/GuildBanAdd.ts
@@ -1,19 +1,29 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
-import type { GuildBan } from "discord.js";
+import { AuditLogEvent } from "discord-api-types/v9";
+import type { GuildBan, User } from "discord.js";
 
 @ApplyOptions<Listener.Options>({
   once: false,
   event: Events.GuildBanAdd,
 })
 export class GuildBanAddListener extends Listener {
-  public async run({ guild, user, reason }: GuildBan) {
-    this.container.client.guildLogger.modAction(guild, {
-      action: "Ban",
+  public async run({ guild, user }: GuildBan) {
+    const auditLogs = await guild.fetchAuditLogs({
+      limit: 10,
+      type: AuditLogEvent.MemberBanAdd,
+    });
+    const log = auditLogs.entries.find(
+      (log) => (log.target as User).id === user.id
+    );
+    if (!log) return;
+    if (log.executor?.id == this.container.client?.user?.id) return;
+
+    return void this.container.client.guildLogger.modAction(guild, {
+      action: "Manual Ban (Not via Evie)",
       target: user,
-      reason: reason
-        ? `${reason} (Ban not made via Evie)`
-        : "No reason provided.",
+      reason: log.reason ? log.reason : `No reason provided.`,
+      moderator: log?.executor || undefined,
     });
   }
 }
