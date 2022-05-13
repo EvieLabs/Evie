fc4aa2ac137dcd7aa30c87e536f67b381838bb28
Tristan Camejo <twisttaan@icloud.com>
2022-05-13 22:22:42 +1000

    feat: no reason, no line

--- src/utils/builders/stringBuilder.ts
+++ src/utils/builders/stringBuilder.ts
@@ -3,7 +3,7 @@ import type { User } from "discord.js";
 export const modAction = (member: User, action: string, reason?: string) => {
   return `**Member**: \`${member.tag}\` (${member.id})
           **Action**: ${action}
-          **Reason**: ${reason ?? "No reason provided."}
+          ${reason ? `**Reason**: ${reason}` : ""}
          `;
 };
 
