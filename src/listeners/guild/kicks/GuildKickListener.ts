39f521b4ef06fbbc80955006b06d892beda89f6c
Tristan <69066026+twisttaan@users.noreply.github.com>
2022-05-20 09:38:35 +1000

    fix: error handling events

--- src/listeners/guild/kicks/GuildKickListener.ts
+++ src/listeners/guild/kicks/GuildKickListener.ts
@@ -1,7 +1,8 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
+import { captureException } from "@sentry/node";
 import { AuditLogEvent } from "discord-api-types/v9";
-import type { GuildMember, User } from "discord.js";
+import { Constants, DiscordAPIError, GuildMember, User } from "discord.js";
 
 @ApplyOptions<Listener.Options>({
   once: false,
@@ -9,21 +10,29 @@ import type { GuildMember, User } from "discord.js";
 })
 export class GuildKickListener extends Listener {
   public async run({ guild, user }: GuildMember) {
-    const auditLogs = await guild.fetchAuditLogs({
-      limit: 10,
-      type: AuditLogEvent.MemberKick,
-    });
-    const log = auditLogs.entries.find(
-      (log) => (log.target as User).id === user.id
-    );
-    if (!log) return;
-    if (log.executor?.id == this.container.client.user?.id) return;
+    try {
+      const auditLogs = await guild.fetchAuditLogs({
+        limit: 10,
+        type: AuditLogEvent.MemberKick,
+      });
+      const log = auditLogs.entries.find(
+        (log) => (log.target as User).id === user.id
+      );
+      if (!log) return;
+      if (log.executor?.id == this.container.client.user?.id) return;
 
-    return void this.container.client.guildLogger.modAction(guild, {
-      action: "Manual Kick (Not via Evie)",
-      target: user,
-      reason: log.reason ? log.reason : `No reason provided.`,
-      moderator: log?.executor || undefined,
-    });
+      return void this.container.client.guildLogger.modAction(guild, {
+        action: "Manual Kick (Not via Evie)",
+        target: user,
+        reason: log.reason ? log.reason : `No reason provided.`,
+        moderator: log?.executor || undefined,
+      });
+    } catch (error) {
+      if (!(error instanceof DiscordAPIError)) return captureException(error);
+
+      if (error.code !== Constants.APIErrors.MISSING_PERMISSIONS) {
+        return;
+      }
+    }
   }
 }
--- src/listeners/guild/timeouts/GuildMemberUpdateListener.ts
+++ src/listeners/guild/timeouts/GuildMemberUpdateListener.ts
@@ -1,7 +1,8 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener } from "@sapphire/framework";
+import { captureException } from "@sentry/node";
 import { AuditLogEvent } from "discord-api-types/v9";
-import type { GuildMember, User } from "discord.js";
+import { Constants, DiscordAPIError, GuildMember, User } from "discord.js";
 
 @ApplyOptions<Listener.Options>({
   once: false,
@@ -9,41 +10,51 @@ import type { GuildMember, User } from "discord.js";
 })
 export class GuildMemberUpdateListener extends Listener {
   public async run(oldMember: GuildMember, { user, guild }: GuildMember) {
-    const auditLogs = await guild.fetchAuditLogs({
-      limit: 10,
-      type: AuditLogEvent.MemberUpdate,
-    });
-
-    const log = auditLogs.entries.find(
-      (log) =>
-        ((log.target as User).id === oldMember.user.id &&
-          log.changes?.some((c) => c.key === "communication_disabled_until")) ??
-        false
-    );
-
-    if (!log) return;
-    if (log.executor?.id == this.container.client.user?.id) return;
-
-    if (!log?.changes) return;
-
-    const timeoutUpdate = log.changes.find(
-      (c) => c.key === "communication_disabled_until"
-    );
-
-    if (!timeoutUpdate) return;
-    const timedOut = !Boolean(timeoutUpdate.old && !timeoutUpdate.new);
-    if (!timedOut)
-      return void this.container.client.guildLogger.modAction(guild, {
-        action: "Un-timeout",
-        target: user,
-        moderator: log?.executor || undefined,
+    try {
+      const auditLogs = await guild.fetchAuditLogs({
+        limit: 10,
+        type: AuditLogEvent.MemberUpdate,
       });
 
-    return void this.container.client.guildLogger.modAction(guild, {
-      action: "Timeout",
-      target: user,
-      reason: log.reason ? log.reason : `No reason provided.`,
-      moderator: log?.executor || undefined,
-    });
+      const log = auditLogs.entries.find(
+        (log) =>
+          ((log.target as User).id === oldMember.user.id &&
+            log.changes?.some(
+              (c) => c.key === "communication_disabled_until"
+            )) ??
+          false
+      );
+
+      if (!log) return;
+      if (log.executor?.id == this.container.client.user?.id) return;
+
+      if (!log?.changes) return;
+
+      const timeoutUpdate = log.changes.find(
+        (c) => c.key === "communication_disabled_until"
+      );
+
+      if (!timeoutUpdate) return;
+      const timedOut = !Boolean(timeoutUpdate.old && !timeoutUpdate.new);
+      if (!timedOut)
+        return void this.container.client.guildLogger.modAction(guild, {
+          action: "Un-timeout",
+          target: user,
+          moderator: log?.executor || undefined,
+        });
+
+      return void this.container.client.guildLogger.modAction(guild, {
+        action: "Timeout",
+        target: user,
+        reason: log.reason ? log.reason : `No reason provided.`,
+        moderator: log?.executor || undefined,
+      });
+    } catch (error) {
+      if (!(error instanceof DiscordAPIError)) return captureException(error);
+
+      if (error.code !== Constants.APIErrors.MISSING_PERMISSIONS) {
+        return;
+      }
+    }
   }
 }
