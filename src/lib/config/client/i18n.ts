3e22b58e11dd46f2ee0f363f300ee1698397d423
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-01 21:48:27 +0000

    feat: guild locale

--- src/lib/config/client/i18n.ts
+++ src/lib/config/client/i18n.ts
@@ -1,4 +1,3 @@
-import { container } from "@sapphire/framework";
 import type { InternationalizationContext } from "@sapphire/plugin-i18next";
 import type { ClientOptions } from "discord.js";
 
@@ -6,15 +5,8 @@ export const i18nOptions: ClientOptions["i18n"] = {
   fetchLanguage: async (context: InternationalizationContext) => {
     if (!context.interactionLocale) return "en-US";
     if (context.interactionLocale === "en") return "en-US"; // Removes the chance of picking en-AU instead of en-US
-
-    let langCode = "en-US";
-
-    container.i18n.languages.forEach((_, languageFolderName) => {
-      if (languageFolderName.startsWith(`${context.interactionLocale}`)) {
-        langCode = languageFolderName;
-      }
-    });
-
-    return langCode;
+    if (context.interactionLocale) return context.interactionLocale;
+    if (context.guild?.preferredLocale) return context.guild.preferredLocale;
+    return "en-US";
   },
 };
