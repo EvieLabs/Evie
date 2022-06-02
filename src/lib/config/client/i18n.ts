b5c4cc0b3cfe04b4142319f84609bf18143bfcf7
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-02 00:13:07 +0000

    fix: temp-flow before lang settings

--- src/lib/config/client/i18n.ts
+++ src/lib/config/client/i18n.ts
@@ -3,9 +3,10 @@ import type { ClientOptions } from "discord.js";
 
 export const i18nOptions: ClientOptions["i18n"] = {
   fetchLanguage: async (context: InternationalizationContext) => {
-    if (!context.interactionLocale) return "en-US";
-    if (context.interactionLocale === "en") return "en-US"; // Removes the chance of picking en-AU instead of en-US
-    if (context.interactionLocale) return context.interactionLocale;
+    if (context.interactionLocale) {
+      if (context.interactionLocale.startsWith("en")) return "en-US"; // Removes the chance of picking en-AU instead of en-US
+      return context.interactionLocale;
+    }
     if (context.guild?.preferredLocale) return context.guild.preferredLocale;
     return "en-US";
   },
