337eb24b7de451c3a5688de39afa708094122404
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-05 04:59:22 +0000

    feat: force vote

---
+++ src/commands/dev/testing/forceVote.ts
@@ -0,0 +1,32 @@
+import { Emojis, EvieEvent } from "#root/Enums";
+import { VotePayload } from "@evie/shapers";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Args, Command } from "@sapphire/framework";
+import type { Message } from "discord.js";
+
+@ApplyOptions<Command.Options>({
+  name: "forcevote",
+  description: "Force a vote on a user",
+  preconditions: ["OwnerOnly"],
+  aliases: ["fv"],
+})
+export class ForceVote extends Command {
+  public override async messageRun(message: Message, args: Args) {
+    const user = await args.pick("user").catch(() => message.author);
+    const test = await args.pick("boolean").catch(() => false);
+
+    const payload = new VotePayload({
+      userSnowflake: user.id,
+      test,
+      serviceName: "`ev!testvote`",
+      voteLink: message.url,
+      emoji: Emojis.slashCommand,
+    });
+
+    await payload.init();
+
+    this.container.client.emit(EvieEvent.Vote, payload);
+
+    await message.reply("Done!");
+  }
+}
