d90446f56eebb296df6cb08902cb354167bd203e
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-11 03:17:33 +0000

    fix: optimize git stats

--- src/classes/Stats.ts
+++ src/classes/Stats.ts
@@ -1,5 +1,6 @@
 import type { CommandStats } from "@prisma/client";
 import { container } from "@sapphire/framework";
+import { execSync } from "node:child_process";
 export class Stats {
   public get users(): number {
     return container.client.guilds.cache.reduce(
@@ -7,6 +8,20 @@ export class Stats {
       0
     );
   }
+
+  public static commitSha = execSync("git rev-parse HEAD").toString().trim();
+
+  public static commitName = execSync("git log -1 --pretty=%an")
+    .toString()
+    .trim();
+
+  public static commitNumber = execSync("git rev-list --all --count")
+    .toString()
+    .trim();
+
+  public static currentBranch = execSync("git rev-parse --abbrev-ref HEAD")
+    .toString()
+    .trim();
 
   public async getCommandStats(): Promise<CommandStats[]> {
     return await container.client.prisma.commandStats.findMany();
