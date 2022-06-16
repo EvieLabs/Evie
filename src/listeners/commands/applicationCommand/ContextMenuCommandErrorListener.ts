faa89fbb85b66c8ec44d1aa0aedc847e898ffdc1
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-16 10:44:20 +0000

    feat: context menu error handler

---
+++ src/listeners/commands/applicationCommand/ContextMenuCommandErrorListener.ts
@@ -0,0 +1,46 @@
+import {
+  EditReplyStatusEmbed,
+  ReplyStatusEmbed,
+} from "#root/classes/EvieEmbed";
+import { ApplyOptions } from "@sapphire/decorators";
+import {
+  ArgumentError,
+  ContextMenuCommandErrorPayload,
+  Events,
+  Listener,
+  ListenerOptions,
+  UserError,
+} from "@sapphire/framework";
+import { resolveKey } from "@sapphire/plugin-i18next";
+import { captureException } from "@sentry/node";
+
+@ApplyOptions<ListenerOptions>({ event: Events.ContextMenuCommandError })
+export class ContextMenuCommandErrorListener extends Listener {
+  public async run(
+    error: Error,
+    { interaction }: ContextMenuCommandErrorPayload
+  ) {
+    if (error instanceof UserError || error instanceof ArgumentError) {
+      return interaction.deferred
+        ? void EditReplyStatusEmbed(false, `${error.message}`, interaction)
+        : void ReplyStatusEmbed(false, `${error.message}`, interaction);
+    } else if (typeof error === "string") {
+      return interaction.deferred
+        ? void EditReplyStatusEmbed(false, error, interaction)
+        : void ReplyStatusEmbed(false, error, interaction);
+    } else {
+      captureException(error);
+      return interaction.deferred
+        ? void EditReplyStatusEmbed(
+            false,
+            await resolveKey(interaction, "errors:commandError"),
+            interaction
+          )
+        : void ReplyStatusEmbed(
+            false,
+            await resolveKey(interaction, "errors:commandError"),
+            interaction
+          );
+    }
+  }
+}
