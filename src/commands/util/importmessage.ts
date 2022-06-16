6c716370bf9d35bc63357a3cf5e55765d026466d
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-16 10:44:41 +0000

    refactor: importmessage

--- src/commands/util/importmessage.ts
+++ src/commands/util/importmessage.ts
@@ -1,14 +1,15 @@
 import { EvieEmbed, ReplyStatusEmbed } from "#root/classes/EvieEmbed";
 import { ImportMessageModal } from "#root/constants/modals";
 import { miscDB } from "#root/utils/database/misc";
-import { informNeedsPerms, PermissionLang } from "#root/utils/misc/perms";
 import { registeredGuilds } from "@evie/config";
+import fetch from "@evie/fetch";
 import { ApplyOptions } from "@sapphire/decorators";
 import {
   ApplicationCommandRegistry,
   Command,
   RegisterBehavior,
 } from "@sapphire/framework";
+import { resolveKey } from "@sapphire/plugin-i18next";
 import * as Sentry from "@sentry/node";
 import { ApplicationCommandType } from "discord-api-types/v9";
 import {
@@ -19,6 +20,7 @@ import {
   MessageActionRow,
   MessageButton,
   MessageComponentInteraction,
+  MessageEmbed,
   ModalSubmitInteraction,
   Permissions,
   Snowflake,
@@ -27,6 +29,7 @@ import {
 } from "discord.js";
 @ApplyOptions<Command.Options>({
   description: "Sends Discord Message JSON as a message",
+  requiredUserPermissions: ["MANAGE_MESSAGES"],
 })
 export class ImportMessage extends Command {
   public override async chatInputRun(
@@ -34,47 +37,33 @@ export class ImportMessage extends Command {
   ): Promise<void> {
     if (!interaction.inCachedGuild()) return;
 
-    const perms: Permissions = interaction.member?.permissions as Permissions;
     const channel = interaction.options.getChannel("channel");
 
-    if (!(channel instanceof TextChannel)) {
-      await ReplyStatusEmbed(
-        false,
-        "Please provide a valid text channel.",
-        interaction
+    if (!(channel instanceof TextChannel))
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:invalidChannelType"
       );
-      return;
-    }
-
-    if (!perms.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
-      return informNeedsPerms(interaction, PermissionLang.MANAGE_MESSAGES);
-    }
 
     if (
       !channel
         .permissionsFor(interaction.member)
         .has(Permissions.FLAGS.SEND_MESSAGES)
-    ) {
-      await ReplyStatusEmbed(
-        false,
-        "You do not have permission to send messages in this channel.",
-        interaction
+    )
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:userNoPerms"
       );
-      return;
-    }
 
     if (
       !interaction.guild.me
         ?.permissionsIn(channel)
         .has(Permissions.FLAGS.SEND_MESSAGES)
-    ) {
-      await ReplyStatusEmbed(
-        false,
-        "I do not have permission to send messages in this channel.",
-        interaction
+    )
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:botNoPerms"
       );
-      return;
-    }
 
     const generatedState = SnowflakeUtil.generate();
 
@@ -84,82 +73,66 @@ export class ImportMessage extends Command {
 
   public override async contextMenuRun(interaction: ContextMenuInteraction) {
     if (!interaction.inCachedGuild()) return;
+    await interaction.deferReply({ ephemeral: true });
 
     const message = interaction.options.getMessage("message", true);
 
-    if (!message) {
-      await ReplyStatusEmbed(
-        false,
-        `Please provide a valid message.`,
-        interaction
-      );
-      return;
-    }
-
-    if (message.author.id !== interaction.client.user?.id) {
-      await ReplyStatusEmbed(
-        false,
-        `Please Provide a message that was sent by me.
-        This context menu is used to edit messages you made me send.
-        For more information, read the [documentation on this feature](https://docs.eviebot.rocks/commands/import-message.html).`,
-        interaction
-      );
-      return;
-    }
-
     if (
+      message.author.id !== interaction.client.user?.id ||
       !(await miscDB.getImportedMessages(interaction.guild)).includes(
         message.id
       )
-    ) {
-      await ReplyStatusEmbed(
-        false,
-        `Please don't try and edit a message that is not user-generated.`,
-        interaction
+    )
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:notImportedMessage"
       );
-      return;
-    }
-
-    const perms: Permissions = interaction.member?.permissions as Permissions;
-
-    if (!perms.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
-      informNeedsPerms(interaction, PermissionLang.MANAGE_MESSAGES);
-      return;
-    }
 
     if (
       !message.channel
         .permissionsFor(interaction.member)
         .has(Permissions.FLAGS.SEND_MESSAGES)
-    ) {
-      await ReplyStatusEmbed(
-        false,
-        "You do not have permission to send messages in this channel.",
-        interaction
+    )
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:userNoPerms"
       );
-      return;
-    }
 
     if (
       !interaction.guild.me
         ?.permissionsIn(message.channel)
         .has(Permissions.FLAGS.SEND_MESSAGES)
-    ) {
-      await ReplyStatusEmbed(
-        false,
-        "I do not have permission to edit messages in this channel.",
-        interaction
+    )
+      throw await resolveKey(
+        interaction,
+        "commands/util/importmessage:botNoPerms"
       );
-      return;
-    }
 
     const generatedState = SnowflakeUtil.generate();
 
-    await interaction.reply({
+    const editLink = await fetch.post<
+      {
+        id: string;
+      },
+      {
+        json: {
+          content: string | null;
+          embeds: MessageEmbed[] | null;
+        };
+      }
+    >("https://api.discord.club/messages/share", {
+      json: {
+        content: message.content || null,
+        embeds: message.embeds || null,
+      },
+    });
+
+    await interaction.editReply({
       embeds: [
         new EvieEmbed().setDescription(
-          `Tip: You can copy and paste the existing message JSON into the "JSON Data Editor" on [Discohook](https://discohook.org/) to easily edit the message.
-          Press continue to continue.`
+          await resolveKey(interaction, "commands/util/importmessage:editTip", {
+            editorLink: `https://discord.club/share/${editLink.id}`,
+          })
         ),
       ],
       components: [
@@ -170,10 +143,13 @@ export class ImportMessage extends Command {
             .setCustomId(`continue_msg_import_${generatedState}`)
         ),
       ],
-      ephemeral: true,
     });
 
-    await this.waitForButton(interaction, generatedState, message);
+    return void (await this.waitForButton(
+      interaction,
+      generatedState,
+      message
+    ));
   }
 
   private async waitForButton(
@@ -236,49 +212,26 @@ export class ImportMessage extends Command {
         ReplyStatusEmbed(false, `Import Timed Out`, interaction);
       })) as ModalSubmitInteraction;
 
-    if (!submit) return;
-    if (!submit.fields) return;
-
     const jsonData = submit.fields.getTextInputValue("json_data");
 
-    if (jsonData) {
-      try {
-        if (existingMessage) {
-          const json = JSON.parse(jsonData);
+    try {
+      const json = JSON.parse(jsonData);
 
-          const message = await existingMessage.edit(json);
+      const message = existingMessage
+        ? await existingMessage.edit(json)
+        : await channel.send(json);
+      if (!existingMessage) await miscDB.addImportedMessage(message);
 
-          return ReplyStatusEmbed(
-            true,
-            `Imported [here](<${message.url}>)!`,
-            submit
-          );
-        } else {
-          const json = JSON.parse(jsonData);
-
-          const message = await channel.send(json);
-          await miscDB.addImportedMessage(message);
-
-          return ReplyStatusEmbed(
-            true,
-            `Imported [here](<${message.url}>)!`,
-            submit
-          );
-        }
-      } catch (e) {
-        Sentry.captureException(e);
-        await ReplyStatusEmbed(
-          false,
-          `Failed to import. For more information, read the [documentation on this feature](https://docs.eviebot.rocks/commands/import-message.html).`,
-          submit
-        );
-      }
-    } else {
-      await ReplyStatusEmbed(
-        false,
-        `Missing JSON Data. For more information, read the [documentation on this feature](https://docs.eviebot.rocks/commands/import-message.html).`,
+      return ReplyStatusEmbed(
+        true,
+        await resolveKey(interaction, "commands/util/importmessage:imported", {
+          messageUrl: message.url,
+        }),
         submit
       );
+    } catch (e) {
+      Sentry.captureException(e);
+      throw e;
     }
   }
 
