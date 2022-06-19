c8d9a2fc97684017ddbfa5c551bd77ef3917dc0f
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-19 11:39:43 +0000

    feat: sentry logging

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import "@sapphire/plugin-logger/register";
 import { RewriteFrames } from "@sentry/integrations";
 import * as Sentry from "@sentry/node";
 import { rootFolder } from "./constants/paths";
+import { getSecret } from "./lib/config";
 
 /** The running EvieClient */
 const client = new EvieClient();
@@ -24,19 +25,23 @@ ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(
 
 container.logger.info(`Root folder: ${rootFolder}`);
 
-if (process.env.SENTRY_URL) {
-  Sentry.init({
-    dsn: process.env.SENTRY_URL,
-    integrations: [
-      new Sentry.Integrations.Modules(),
-      new Sentry.Integrations.FunctionToString(),
-      new Sentry.Integrations.LinkedErrors(),
-      new Sentry.Integrations.Console(),
-      new Sentry.Integrations.Http({ breadcrumbs: true, tracing: true }),
-      new RewriteFrames({ root: rootFolder }),
-    ],
-  });
-}
+Sentry.init({
+  dsn: process.env.SENTRY_URL,
+  integrations: [
+    new Sentry.Integrations.Modules(),
+    new Sentry.Integrations.FunctionToString(),
+    new Sentry.Integrations.LinkedErrors(),
+    new Sentry.Integrations.Console(),
+    new Sentry.Integrations.Http({ breadcrumbs: true, tracing: true }),
+    new RewriteFrames({ root: rootFolder }),
+  ],
+  beforeSend: (event) => {
+    if (getSecret("NODE_ENV", false) === "production") return event;
+    container.logger.error(event);
+    return null;
+  },
+  onFatalError: (error: Error) => container.logger.fatal(error),
+});
 
 client.on("debug", (m) => container.logger.debug(m));
 
