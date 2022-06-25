e164bac77de26942b62205573fe2cd78ecefd3ef
tristan <69066026+twisttaan@users.noreply.github.com>
2022-06-25 01:38:48 +0000

    refactor: split ping command

--- services/backend/src/commands/System/ping.ts
+++ services/backend/src/commands/System/ping.ts
@@ -1,21 +1,7 @@
-import { EvieEmbed } from "#root/classes/EvieEmbed";
-import { Emojis } from "#root/Enums";
 import { removeIndents } from "#root/utils/builders/stringBuilder";
-import { inlineCode, time } from "@discordjs/builders";
-import { registeredGuilds } from "@evie/config";
-import type {
-  OfflineServerResponse,
-  OnlineServerResponse,
-} from "@evie/interfaces";
-import { fetch, FetchResultTypes } from "@sapphire/fetch";
-import {
-  ApplicationCommandRegistry,
-  Command,
-  RegisterBehavior,
-} from "@sapphire/framework";
-import { captureException } from "@sentry/node";
+import { Command } from "@sapphire/framework";
 import { blue, green, magenta } from "colorette";
-import { CommandInteraction, Message, MessageAttachment } from "discord.js";
+import type { Message } from "discord.js";
 
 export class Ping extends Command {
   public constructor(context: Command.Context, options: Command.Options) {
@@ -23,120 +9,8 @@ export class Ping extends Command {
       ...options,
       name: "ping",
       aliases: ["pong"],
-      description:
-        "Use the text command to check the bot's latency. Use the slash command to ping anything.",
+      description: "System status check.",
     });
-  }
-
-  public override async chatInputRun(interaction: CommandInteraction) {
-    const destination = interaction.options.getString("destination");
-
-    if (!destination) throw "You must specify a destination to ping.";
-
-    await interaction.deferReply();
-
-    const mcServer = await this.pingMinecraftServer(destination);
-
-    if (mcServer) return void this.handleMinecraftServer(mcServer, interaction);
-
-    throw "Couldn't resolve your destination. (currently only supporting Minecraft servers until we make our proxy service)";
-  }
-
-  private async handleMinecraftServer(
-    mcServer: OnlineServerResponse | OfflineServerResponse,
-    interaction: CommandInteraction
-  ) {
-    const { hostname } = mcServer;
-
-    const embeds: EvieEmbed[] = [];
-    const files: MessageAttachment[] = [];
-
-    switch (mcServer.online) {
-      case true: {
-        const { motd, icon, ip, port, protocol, debug } = mcServer;
-        const { cachetime } = debug;
-        const { html } = motd;
-
-        const embed = new EvieEmbed();
-
-        try {
-          const { data: image } = await this.container.client.park.post(
-            "/minecraft/motd",
-            {
-              lines: html,
-            },
-            {
-              responseType: "arraybuffer",
-            }
-          );
-
-          files.push(new MessageAttachment(Buffer.from(image), `motd.png`));
-          embed.setImage(`attachment://motd.png`);
-
-          const iconBuffer = Buffer.from(
-            icon.replace("data:image/png;base64,", ""),
-            "base64"
-          );
-          files.push(new MessageAttachment(iconBuffer, `icon.png`));
-          embed.setThumbnail(`attachment://icon.png`);
-        } catch (e) {
-          captureException(e);
-        }
-
-        embed.setTitle(hostname);
-
-        const basicInfo = [
-          `Players: ${inlineCode(
-            `${mcServer.players.online}/${mcServer.players.max}`
-          )}`,
-          `Hostname: ${inlineCode(hostname)}`,
-          `Version: ${inlineCode(mcServer.version)}`,
-        ].map((line) => `${Emojis.bulletPoint} ${line}`);
-
-        const advancedInfo = [
-          `IP: ${inlineCode(ip)}`,
-          `Port: ${inlineCode(port.toString())}`,
-          `Protocol Version: ${inlineCode(protocol.toString())}`,
-          `Data from: ${
-            cachetime === 0
-              ? time(new Date(), "R")
-              : time(new Date(cachetime * 1000), "R")
-          } 
-        `,
-        ].map((line) => `${Emojis.bulletPoint} ${line}`);
-
-        embed.addFields([
-          {
-            name: "Basic Info",
-            value: basicInfo.join("\n"),
-          },
-          {
-            name: "Advanced Info",
-            value: advancedInfo.join(`\n`),
-          },
-        ]);
-
-        embeds.push(embed);
-      }
-    }
-
-    await interaction.editReply({ embeds, files });
-  }
-
-  private async pingMinecraftServer(
-    hostname: string
-  ): Promise<OnlineServerResponse | OfflineServerResponse | null> {
-    try {
-      const data = await fetch<OnlineServerResponse | OfflineServerResponse>(
-        `https://api.mcsrvstat.us/2/${hostname}`,
-        FetchResultTypes.JSON
-      );
-      return data.online
-        ? (data as OnlineServerResponse)
-        : (data as OfflineServerResponse);
-    } catch (e) {
-      return null;
-    }
   }
 
   public override async messageRun(message: Message) {
@@ -152,29 +26,5 @@ export class Ping extends Command {
     \`\`\``);
 
     return msg.edit(content);
-  }
-
-  public override registerApplicationCommands(
-    registry: ApplicationCommandRegistry
-  ) {
-    registry.registerChatInputCommand(
-      {
-        name: this.name,
-        description: '"Ping" literally anything.',
-        options: [
-          {
-            name: "destination",
-            description:
-              "The destination to ping (Minecraft Server, Website, IP, etc.)",
-            type: "STRING",
-            required: true,
-          },
-        ],
-      },
-      {
-        guildIds: registeredGuilds,
-        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
-      }
-    );
   }
 }
---
+++ services/backend/src/commands/Utilities/mcserver.ts
@@ -0,0 +1,169 @@
+import { EvieEmbed } from "#root/classes/EvieEmbed";
+import { Emojis } from "#root/Enums";
+import { inlineCode, time } from "@discordjs/builders";
+import { registeredGuilds } from "@evie/config";
+import type {
+  OfflineServerResponse,
+  OnlineServerResponse,
+} from "@evie/interfaces";
+import { fetch, FetchResultTypes } from "@sapphire/fetch";
+import {
+  ApplicationCommandRegistry,
+  Command,
+  RegisterBehavior,
+} from "@sapphire/framework";
+import { captureException } from "@sentry/node";
+import { CommandInteraction, MessageAttachment } from "discord.js";
+
+export class MinecraftServer extends Command {
+  public constructor(context: Command.Context, options: Command.Options) {
+    super(context, {
+      ...options,
+      name: "mcserver",
+      description: "Get an insight on a Minecraft server.",
+    });
+  }
+
+  public override async chatInputRun(interaction: CommandInteraction) {
+    const destination = interaction.options.getString("destination");
+
+    if (!destination) throw "You must specify a destination to ping.";
+
+    const ephemeral = !interaction.options.getBoolean("show") ?? false;
+
+    await interaction.deferReply({ ephemeral });
+
+    const mcServer = await this.pingMinecraftServer(destination);
+
+    if (mcServer) return void this.handleMinecraftServer(mcServer, interaction);
+
+    throw "Couldn't resolve your destination. Does this server exist? Is it offline?";
+  }
+
+  private async handleMinecraftServer(
+    mcServer: OnlineServerResponse | OfflineServerResponse,
+    interaction: CommandInteraction
+  ) {
+    const { hostname } = mcServer;
+
+    const embeds: EvieEmbed[] = [];
+    const files: MessageAttachment[] = [];
+
+    switch (mcServer.online) {
+      case true: {
+        const { motd, icon, ip, port, protocol, debug } = mcServer;
+        const { cachetime } = debug;
+        const { html } = motd;
+
+        const embed = new EvieEmbed();
+
+        try {
+          const { data: image } = await this.container.client.park.post(
+            "/minecraft/motd",
+            {
+              lines: html,
+            },
+            {
+              responseType: "arraybuffer",
+            }
+          );
+
+          files.push(new MessageAttachment(Buffer.from(image), `motd.png`));
+          embed.setImage(`attachment://motd.png`);
+
+          const iconBuffer = Buffer.from(
+            icon.replace("data:image/png;base64,", ""),
+            "base64"
+          );
+          files.push(new MessageAttachment(iconBuffer, `icon.png`));
+          embed.setThumbnail(`attachment://icon.png`);
+        } catch (e) {
+          captureException(e);
+        }
+
+        embed.setTitle(hostname);
+
+        const basicInfo = [
+          `Players: ${inlineCode(
+            `${mcServer.players.online}/${mcServer.players.max}`
+          )}`,
+          `Hostname: ${inlineCode(hostname)}`,
+          `Version: ${inlineCode(mcServer.version)}`,
+        ].map((line) => `${Emojis.bulletPoint} ${line}`);
+
+        const advancedInfo = [
+          `IP: ${inlineCode(ip)}`,
+          `Port: ${inlineCode(port.toString())}`,
+          `Protocol Version: ${inlineCode(protocol.toString())}`,
+          `Data from: ${
+            cachetime === 0
+              ? time(new Date(), "R")
+              : time(new Date(cachetime * 1000), "R")
+          } 
+        `,
+        ].map((line) => `${Emojis.bulletPoint} ${line}`);
+
+        embed.addFields([
+          {
+            name: "Basic Info",
+            value: basicInfo.join("\n"),
+          },
+          {
+            name: "Advanced Info",
+            value: advancedInfo.join(`\n`),
+          },
+        ]);
+
+        embeds.push(embed);
+      }
+    }
+
+    await interaction.editReply({ embeds, files });
+  }
+
+  private async pingMinecraftServer(
+    hostname: string
+  ): Promise<OnlineServerResponse | OfflineServerResponse | null> {
+    try {
+      const data = await fetch<OnlineServerResponse | OfflineServerResponse>(
+        `https://api.mcsrvstat.us/2/${hostname}`,
+        FetchResultTypes.JSON
+      );
+      return data.online
+        ? (data as OnlineServerResponse)
+        : (data as OfflineServerResponse);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  public override registerApplicationCommands(
+    registry: ApplicationCommandRegistry
+  ) {
+    registry.registerChatInputCommand(
+      {
+        name: this.name,
+        description: this.description,
+        options: [
+          {
+            name: "destination",
+            description:
+              "The hostname or IP of the server to ping. (e.g. hypixel.net)",
+            type: "STRING",
+            required: true,
+          },
+          {
+            name: "show",
+            description: "Send the message non-ephemerally",
+            type: "BOOLEAN",
+            required: false,
+          },
+        ],
+      },
+      {
+        guildIds: registeredGuilds,
+        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+      }
+    );
+  }
+}
