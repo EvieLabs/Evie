782637b0baa1164d2c30ee45a7d38b83ec7aeb15
tristan <69066026+twisttaan@users.noreply.github.com>
2022-07-13 11:50:59 +1000

    fix: allow namespaces (#167)

--- services/bot/src/lib/internal/structures/modules/Module.ts
+++ services/bot/src/lib/internal/structures/modules/Module.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-namespace */
 import { container, Piece } from "@sapphire/framework";
 import { captureException } from "@sentry/node";
 import type { Guild, MessageEmbed } from "discord.js";
