8491451bbe5e8d2d6e27953be1a0dd6cddb6857e
tristan <69066026+twisttaan@users.noreply.github.com>
2022-07-21 01:52:20 +0000

    feat: pat

---
+++ services/bot/src/listeners/modules/Eviecord/Pat.ts
@@ -0,0 +1,19 @@
+import { ApplyOptions } from "@sapphire/decorators";
+import { Events, Listener } from "@sapphire/framework";
+import type { Message } from "discord.js";
+
+const PAT_USER_ID = "193988283645034496";
+
+@ApplyOptions<Listener.Options>({
+	once: false,
+	event: Events.MessageCreate,
+})
+export class Pat extends Listener {
+	public run(message: Message) {
+		if (message.author.id !== PAT_USER_ID || this.container.client.kennel.guildId) return;
+		if (!(message.content.toLowerCase().includes("good morning") || message.content.toLowerCase().includes("gm")))
+			return;
+
+		void message.reply("morning pat");
+	}
+}
