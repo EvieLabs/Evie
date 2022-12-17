db57b26990938cc6e7e898c6af19332682f35b3e
Tristan Camejo <contact@tristancamejo.com>
2022-12-17 23:56:46 +1100

    lint(tail-bot-client): *

--- apps/tail-bot-client/src/commands/create.ts
+++ apps/tail-bot-client/src/commands/create.ts
@@ -1,50 +1,50 @@
-import { ApplicationCommandOptionType, MessageFlags } from 'discord-api-types/v10';
-import type { Command } from 'disploy';
-import { RecordSchema } from '../lib/schemas';
-import { TailClient } from '../lib/trpcClient';
+import { ApplicationCommandOptionType, MessageFlags } from "discord-api-types/v10";
+import type { Command } from "disploy";
+import { RecordSchema } from "../lib/schemas";
+import { TailClient } from "../lib/trpcClient";
 
 export default {
-	name: 'create',
-	description: 'create a webhook',
+	name: "create",
+	description: "create a webhook",
 	options: [
 		{
-			name: 'tag',
-			description: 'tag for the webhook',
+			name: "tag",
+			description: "tag for the webhook",
 			type: ApplicationCommandOptionType.String,
 			required: true,
 		},
 		{
-			name: 'metadata',
-			description: 'json metadata for the webhook',
+			name: "metadata",
+			description: "json metadata for the webhook",
 			type: ApplicationCommandOptionType.String,
 			required: false,
 		},
 	],
 
 	async run(interaction) {
-		const tag = interaction.options.getString('tag');
-		const metadata = interaction.options.getString('metadata', true);
-		
-		let metadataParsed = {}
+		const tag = interaction.options.getString("tag");
+		const metadata = interaction.options.getString("metadata", true);
 
+		let metadataParsed = {};
 
 		if (metadata) {
-		try {
-			metadataParsed=RecordSchema.parse(JSON.parse(metadata));
-		} catch (error) {
-			return void interaction.reply({
-				content: 'Invalid metadata',
-				flags: MessageFlags.Ephemeral,
-			});
-		}}
+			try {
+				metadataParsed = RecordSchema.parse(JSON.parse(metadata));
+			} catch (error) {
+				return void interaction.reply({
+					content: "Invalid metadata",
+					flags: MessageFlags.Ephemeral,
+				});
+			}
+		}
 
 		await interaction.deferReply({
 			ephemeral: true,
 		});
 
 		const tail = TailClient(interaction);
-	
-		const webhook =await tail.webhooks.create.mutate({
+
+		const webhook = await tail.webhooks.create.mutate({
 			metadata: metadataParsed,
 			tag,
 		});
@@ -52,11 +52,10 @@ export default {
 		return void interaction.editReply({
 			embeds: [
 				{
-					title: 'Webhook created',
+					title: "Webhook created",
 					description: `🔗 ${webhook.url}`,
 				},
 			],
-
 		});
 	},
 } satisfies Command;
