c2d1c3890f5d7c7c4edc93487aa7bc5b460f2ec9
Tristan Camejo <contact@tristancamejo.com>
2022-12-18 17:45:50 +1100

    feat(tail): send services on health route

--- apps/tail/src/routes/_app.ts
+++ apps/tail/src/routes/_app.ts
@@ -1,10 +1,36 @@
+import { container } from "tsyringe";
 import { z } from "zod";
+import { ServiceManager } from "../managers/ServiceManager";
 import { publicProcedure, router } from "../trpc";
 import { webhooksRouter } from "./webhooks";
 
 export const appRouter = router({
 	webhooks: webhooksRouter,
-	health: publicProcedure.output(z.string()).query(() => "ok"),
+	health: publicProcedure
+		.output(
+			z.object({
+				services: z.array(
+					z.object({
+						name: z.string(),
+						ping: z.number(),
+					}),
+				),
+			}),
+		)
+		.query(() => {
+			const services: { name: string; ping: number }[] = [];
+
+			for (const [uuid, service] of container.resolve(ServiceManager).services) {
+				services.push({
+					name: uuid,
+					ping: service.ping,
+				});
+			}
+
+			return {
+				services,
+			};
+		}),
 });
 
 export type AppRouter = typeof appRouter;
